a96c67312ef43c97bff8dbcc4675b920bfe92880
Joshument <74075169+Joshument@users.noreply.github.com>
2022-06-15 12:37:07 -0400

    Remove ambiguity issues and make state option optional

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use serenity::model::prelude::*;
 use serenity::prelude::*;
 use crate::application_command::ApplicationCommandInteractionDataOptionValue;
 use reqwest;
-use serde::Deserialize;
+use serde::{self,Deserialize};
 use dotenv;
 
 const KELVIN_OFFSET: f32 = 273.15;
@@ -30,21 +30,34 @@ struct Zero {
     icon: String,
 }
 
+/*
 #[derive(Deserialize)]
 struct ZeroGeocode {
     lat: f32,
     lon: f32,
+} */
+
+#[derive(Deserialize)]
+struct AlertInfo {
+    description: String,
 }
 
 #[derive(Deserialize)]
 struct WeatherResponse {
     current: Current,
+    #[serde(default)]
+    alerts: Option<Vec<AlertInfo>>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct GeocodeResponse {
-    zero: ZeroGeocode
+    lat: f32,
+    lon: f32,
+    name: String,
+    state: String,
+    country: String,
 }
+
 struct Handler;
 
 #[async_trait]
@@ -55,25 +68,33 @@ impl EventHandler for Handler {
         if let Interaction::ApplicationCommand(command) = interaction {
             match command.data.name.as_str() {
                 "weather" => {
-                    let city = if let ApplicationCommandInteractionDataOptionValue::String(num) 
-                        = command.data.options.get(0).expect("did not get parameter").resolved.as_ref().expect("idk") {num} else {"idk"};
+                    // city should never fail as city is a required parameter
+                    let city = if let ApplicationCommandInteractionDataOptionValue::String(city) = command.data.options
+                        .iter().find(|x| x.name == "city").unwrap()
+                        .resolved.as_ref().expect("idk") { city } else { "idk" };
+
+                    let state = if command.data.options.len() == 2 { city } else {
+                        let temp = if let ApplicationCommandInteractionDataOptionValue::String(state) = command.data.options
+                            .iter().find(|x| x.name == "state").unwrap()
+                            .resolved.as_ref().expect("idk") { state } else { "idk" };
+                        temp
+                    };
 
                     // let latitude: f32 = command.data.options.get(0).expect("didn't get the parameter!").resolved.as_ref().expect("idk");
-                    let state = if let ApplicationCommandInteractionDataOptionValue::String(num) 
-                        = command.data.options.get(1).expect("did not get parameter").resolved.as_ref().expect("idk") {num} else {"idk"};
 
-                    let country = if let ApplicationCommandInteractionDataOptionValue::String(num) 
-                        = command.data.options.get(2).expect("did not get parameter").resolved.as_ref().expect("idk") {num} else {"idk"};
+                    let country = if let ApplicationCommandInteractionDataOptionValue::String(country) = command.data.options
+                        .iter().find(|x| x.name == "country").unwrap()
+                        .resolved.as_ref().expect("idk") { country } else { "idk" };
 
                     let geocode_response = match reqwest::get(
                         format!(
-                            "http://api.openweathermap.org/geo/1.0/direct?q={},{},{}&limit=1&appid={}",
+                            "http://api.openweathermap.org/geo/1.0/direct?q={},{},{}&limit=10&appid={}",
                             city,
-                            state,
+                            if state == city {""} else {state},
                             country,
                             api_key
                         )
-                    ).await.expect("Failed to get API response!").json::<GeocodeResponse>().await {
+                    ).await.expect("Failed to get API response!").json::<Vec<GeocodeResponse>>().await {
                         Ok(yea) => yea,
                         Err(_) => {
                             command.create_interaction_response(&ctx.http, |response| {
@@ -87,6 +108,31 @@ impl EventHandler for Handler {
                             panic!("Invalid Location!")
                         }
                     };
+
+                    println!("{:#?}", geocode_response);
+                    if geocode_response.len() != 1 {
+                        let mut city_options = String::new();
+
+                        for x in geocode_response {
+                            city_options.push_str(&format!("{}, {}, {}\n", x.name, x.state, x.country));
+                        }
+
+                        command.create_interaction_response(&ctx.http, |response| {
+                            response
+                                .kind(InteractionResponseType::ChannelMessageWithSource)
+                                .interaction_response_data(|message| {
+                                    message.content(format!(
+                                        "More than 1 city found! Please be more specific on one of the following options:\n{}",
+                                        city_options
+                                    ))
+                                })
+                        }).await.expect("Failed to send message!");
+
+                        panic!("Ambiguous Location!")
+                    }
+                    
+                    // This should never fail, as if it hasn't already failed there would be at least 1 element.
+                    let geocode_response = geocode_response.get(0).unwrap();
                     
                     /*.unwrap_or_else(|_| {
                         command.create_interaction_response(&ctx.http, |response| {
@@ -102,9 +148,9 @@ impl EventHandler for Handler {
 
                     let api_response = reqwest::get(
                         format!(
-                            "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&appid={}&exclude=minutely,hourly,daily,alerts", 
-                            geocode_response.zero.lat, 
-                            geocode_response.zero.lon, 
+                            "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&appid={}&exclude=minutely,hourly,daily", 
+                            geocode_response.lat, 
+                            geocode_response.lon, 
                             api_key
                         )
                     ).await.expect("Failed to get weather!").json::<WeatherResponse>().await.expect("Failed to deserialize JSON");
@@ -121,7 +167,11 @@ impl EventHandler for Handler {
                                 .kind(InteractionResponseType::ChannelMessageWithSource)
                                 .interaction_response_data(|message| {
                                     message.embed(|embed| {
-                                        embed.title(format!("Weather for {}, {}", city, state))
+                                        embed.title(format!(
+                                            "Weather for {}, {}", 
+                                            geocode_response.name,
+                                            if geocode_response.state == geocode_response.name { &geocode_response.country } else { &geocode_response.state }
+                                        ))
                                             .description(format!("{}", api_response.current.weather.zero.description))
                                             .image(format!("http://openweathermap.org/img/wn/{}@2x.png", api_response.current.weather.zero.icon))
                                             .fields(vec![
@@ -135,6 +185,17 @@ impl EventHandler for Handler {
                                                     feels_like_celsius,
                                                     feels_like_fahrenheit
                                                 ), true),
+                                                (
+                                                    "Weather Alerts",
+                                                    api_response.alerts.unwrap_or_else(|| 
+                                                        vec![
+                                                            AlertInfo{
+                                                                description: String::from("There are no current weather alerts for this area.")
+                                                            }
+                                                        ]
+                                                    ).get(0).unwrap().description.clone(),
+                                                    false
+                                                ),
                                             ])
                                     })
                                 })
@@ -160,12 +221,6 @@ impl EventHandler for Handler {
         ); */
 
         let mut commands: Vec<Result<ApplicationCommand, _>> = Vec::new();
-        
-        commands.push(
-            ApplicationCommand::create_global_application_command(&ctx.http, |command| {
-                command.name("sendembed").description("send a test embed")
-            }).await
-        );
 
         commands.push(
             ApplicationCommand::create_global_application_command(&ctx.http, |command| {
@@ -181,17 +236,17 @@ impl EventHandler for Handler {
                     })
                     .create_option(|option| {
                         option
-                            .name("state")
-                            .description("State or Province of the location")
+                            .name("country")
+                            .description("Country of the location")
                             .kind(ApplicationCommandOptionType::String)
                             .required(true)
                     })
                     .create_option(|option| {
                         option
-                            .name("country")
-                            .description("Country of the location")
+                            .name("state")
+                            .description("State or Province of the location")
                             .kind(ApplicationCommandOptionType::String)
-                            .required(true)
+                            .required(false)
                     })
             }).await
         );
@@ -211,8 +266,7 @@ async fn main() {
     // Configure the client with your Discord bot token in the environment.
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
     let intents = GatewayIntents::GUILD_MESSAGES
-        | GatewayIntents::DIRECT_MESSAGES
-        | GatewayIntents::MESSAGE_CONTENT;
+        | GatewayIntents::DIRECT_MESSAGES;
     let mut client =
         Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
 
